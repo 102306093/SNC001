4a1bdcfb70b894a292c0a29cab554edeea051fed
102306093 <102306093@nccu.edu.tw>
2016-06-01 17:33:49 +0800

    Revert "user_function"

--- db/migrate/20160524110522_add_name_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddNameToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :username, :string
-    add_column :users, :address, :string
-    add_column :users, :phone, :string
-  end
-end
