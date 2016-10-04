7880d3988e0b03f5436516090bcf8f3f92c4f046
nick66551 <john831118@gmail.com>
2016-10-04 12:11:37 +0000

    about us

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'order_items/update'
   get 'order_items/destroy'
   get 'products/productmap'
-
+  get 'home/aboutus'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
