eed2dff6eab6b9d40dbcf8c0091149a0111acc67
nick66551 <john831118@gmail.com>
2016-12-06 17:15:38 +0000

    User center v2 with translation and FB log in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,26 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def edit
+    if current_user
   @customer = Customer.where('email = ?', current_user.email )
+    end
+    
   end
+  
+   def facebook
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        session[:user_id] = @user.uid
+        sign_in_and_redirect @user
+    end
 
   def account_update_params
     params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :admin, :username, :u_convenienceaddress, :u_homeaddress, :u_phone)
+  end
+    protected
+   def after_update_path_for(resource)
+      edit_registration_path(resource)
+  end
+  def update_resource(resource, params)
+    resource.update_without_password(params)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
          
   def self.current_user
     Thread.current[:current_user]
@@ -11,4 +11,17 @@ class User < ActiveRecord::Base
   def self.current_user=(usr)
     Thread.current[:current_user] = usr
   end
+  
+  def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.username = auth.info.name
+        user.encrypted_password= Devise.friendly_token[0,20]
+        user.save!
+      end
+  
+  end
+  
 end
